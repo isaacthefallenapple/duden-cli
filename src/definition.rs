74f2cf5b8d463519a54e4c81dfe30cc9fd620e7d
isaacthefallenapple <isaacthefallenapple@gmail.com>
2023-05-31 13:27:08 +0200

    parse all meanings as simple for now

--- src/definition.rs
+++ src/definition.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
-use scraper::{Html, Selector};
+use scraper::{ElementRef, Html, Selector};
 
+#[derive(Debug)]
 pub struct Definition<'a> {
     title: &'a str,
     // TODO: implement this
@@ -10,15 +11,17 @@ pub struct Definition<'a> {
 }
 
 impl Definition<'_> {
-    pub fn title(&self) -> &str {
-        self.title
+    pub fn title(&self) -> String {
+        let mut title = String::with_capacity(self.title.len());
+        crate::fmt::write_no_shys(&mut title, self.title).unwrap();
+        title
     }
 }
 
 impl<'html> Definition<'html> {
     pub fn parse(html: &'html Html) -> Result<Self> {
         let title_selector = Selector::parse("h1 > span").unwrap();
-        let _meanings_selector = Selector::parse("#bedeutungen").unwrap();
+        let meanings_selector = Selector::parse("#bedeutungen .enumeration__item").unwrap();
 
         let title = html
             .select(&title_selector)
@@ -26,10 +29,13 @@ impl<'html> Definition<'html> {
             .and_then(|title| title.text().next())
             .expect("definition doesn't have title");
 
-        Ok(Self {
-            title,
-            meanings: Vec::new(),
-        })
+        let mut meanings = Vec::new();
+
+        for meaning in html.select(&meanings_selector) {
+            meanings.push(Meaning::parse(meaning)?);
+        }
+
+        Ok(Self { title, meanings })
     }
 }
 
@@ -44,13 +50,38 @@ struct Spelling<'a> {
     related: Vec<&'a str>,
 }
 
+#[derive(Default, Debug)]
 struct SimpleMeaning<'a> {
-    text: &'a str,
+    text: String,
     usage: Option<&'a str>,
     example: Option<Vec<&'a str>>,
 }
 
+impl<'html> SimpleMeaning<'html> {
+    fn parse(html: ElementRef<'html>) -> Result<Self> {
+        let text_selector = Selector::parse(".enumeration__text").unwrap();
+        let text = html
+            .select(&text_selector)
+            .next()
+            .map(|text| text.text().collect())
+            .ok_or(anyhow::anyhow!("simple meaning has no text"))?;
+
+        Ok(Self {
+            text,
+            ..Self::default()
+        })
+    }
+}
+
+#[derive(Debug)]
 enum Meaning<'a> {
     Simple(SimpleMeaning<'a>),
     Complex(Vec<SimpleMeaning<'a>>),
+}
+
+impl<'html> Meaning<'html> {
+    fn parse(html: ElementRef<'html>) -> Result<Self> {
+        let simple_meaning = SimpleMeaning::parse(html)?;
+        Ok(Self::Simple(simple_meaning))
+    }
 }
