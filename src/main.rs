1193a0e08e353684ac8d121c8f7656fc32c9f50c
isaacthefallenapple <isaacthefallenapple@gmail.com>
2023-05-31 12:25:59 +0200

    little error handling

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,28 @@
 use std::env;
 
+use anyhow::Result;
+
 mod fetch;
 mod search;
 
-fn main() {
+fn main() -> Result<()> {
     let mut args = env::args().skip(1);
 
     let Some(subcommand) = args.next() else {
-        return;
+        anyhow::bail!("no command given");
     };
 
     match &*subcommand {
         "search" => {
             let Some(argument) = args.next() else {
-                return;
+                anyhow::bail!("missing argument for `search` command");
             };
-            search::search(&argument);
+            search::search(&argument)?;
         }
         _ => {
-            eprintln!("Unknown command: \"{subcommand}\"");
-            return;
+            anyhow::bail!("unknown command: `{}`", subcommand);
         }
     }
 
-    println!("{}", subcommand);
+    Ok(())
 }
