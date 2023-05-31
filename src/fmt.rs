e488c169f04faf4eea0b93bebe2fd388967260bd
isaacthefallenapple <isaacthefallenapple@gmail.com>
2023-05-31 16:34:47 +0200

    rename write_no_shys -> write_without_shys

--- src/fmt.rs
+++ src/fmt.rs
@@ -1,6 +1,6 @@
 use std::fmt;
 
-pub fn write_no_shys(mut w: impl fmt::Write, s: &str) -> fmt::Result {
+pub fn write_without_shys(mut w: impl fmt::Write, s: &str) -> fmt::Result {
     s.chars()
         .filter(|&c| c != '\u{ad}')
         .try_for_each(|c| w.write_char(c))
